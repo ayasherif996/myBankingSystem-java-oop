2192f4904e5358694c0dc1baffdfbb1d6c69ce89
ayasherif996 <ayasherifmohamed@gmail.com>
2023-07-30 17:49:14 +0300

    Deleted src

--- src/accounts/WithdrawTransaction.java
+++
@@ -1,8 +0,0 @@
-package accounts;
-
-public class WithdrawTransaction extends Transaction {
-    @Override
-    public void perform_trasaction() {
-        System.out.println("Perform Withdraw Transaction");
-    }
-}
